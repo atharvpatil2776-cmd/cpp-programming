dc3dbad507401b7b25d7652c9933149185e27bde
Atharv-Ajay-Patil <atharvpatil2776@gmail.com>
2026-07-14 15:44:56 +0530

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b ;
+cout<<"enter first no";
+cin>>a;
+cout<<"enter second no";
+cin>>b;
+cout<<"addition ="<<a+b<<endl;
+cout<<"substraction ="<< a-b << endl;
+cout<<"multiplication ="<< a*b << endl;
+cout<<"divition ="<< a/b << endl;
+cout<<"modulus ="<< a%b ;
+}
+
