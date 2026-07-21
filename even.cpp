7c049edc874cceeb5ea6d0e7bf280a79a37f9f13
Atharv-Ajay-Patil <atharvpatil2776@gmail.com>
2026-07-21 14:43:29 +0530

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter no"<<endl;
+    cin>>a;
+if(a%2==0){
+    cout<<"the no is even"<<endl;
+}
+else{
+    cout<<"the no is odd"<<endl;
+
+}
+return 0;
+}
