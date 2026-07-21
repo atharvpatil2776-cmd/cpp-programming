39411781524b7aef81f1b58236ae8aa4d62b6419
Atharv-Ajay-Patil <atharvpatil2776@gmail.com>
2026-07-21 14:57:13 +0530

    Add files via upload

---
+++ largest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin>>a;
+    cin>>b;
+    if (a>b){
+        cout<<"a is bigger"<<endl;
+    }
+    else{
+        cout<<"b is bigger";
+    }
+    return 0;
+}
